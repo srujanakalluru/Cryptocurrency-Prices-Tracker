2cda89d63a35f0ac7fb5975ff02368b44359c71f
creationk <creationk@users.noreply.github.com>
2022-07-24 12:21:36 +0530

    Initial Commit

---
+++ src/main/java/com/CryptoPricesTrackerApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoPricesTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoPricesTrackerApplication.class, args);
+	}
+
+}
