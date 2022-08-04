9cb8f263376927df2f84ba6c8f5109bb2e6df444
creationk <creationk@users.noreply.github.com>
2022-08-04 20:10:50 +0530

    Adding validations api

--- src/main/java/com/controller/CryptoPricesTrackingController.java
+++ src/main/java/com/controller/CryptoPricesTrackingController.java
@@ -4,15 +4,18 @@ import com.entity.BitcoinData;
 import com.service.CryptoPricesTrackingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.Pattern;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/prices")
+@Validated
 public class CryptoPricesTrackingController {
     CryptoPricesTrackingService service;
 
@@ -22,7 +25,7 @@ public class CryptoPricesTrackingController {
     }
 
     @GetMapping(value = "/btc")
-    public ResponseEntity<List<BitcoinData>> getPriceDetails(@RequestParam("date") String date, @RequestParam(required = false) Integer limit, @RequestParam(required = false) Integer offset) {
+    public ResponseEntity<List<BitcoinData>> getPriceDetails(@Pattern(regexp = "^([0-2][0-9]|(3)[0-1])(-)(((0)[0-9])|((1)[0-2]))(-)\\d{4}$") @RequestParam("date") String date, @RequestParam(required = false) Integer limit, @RequestParam(required = false) Integer offset) {
         return ResponseEntity.ok(service.getPriceDetails(date, limit, offset));
     }
 }
