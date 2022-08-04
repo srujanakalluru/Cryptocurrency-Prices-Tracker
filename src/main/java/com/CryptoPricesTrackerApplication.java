88e3befe75f1c43691c188305f258388b5c86a5d
creationk <creationk@users.noreply.github.com>
2022-08-04 13:24:06 +0530

    Switch to undertow from tomcat

--- src/main/java/com/CryptoPricesTrackerApplication.java
+++ src/main/java/com/CryptoPricesTrackerApplication.java
@@ -1,9 +1,7 @@
 package com;
 
-import com.errorhandling.GlobalExceptionHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
