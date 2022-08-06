160dba95dacda38907acbaf497096925ed62f886
creationk <creationk@users.noreply.github.com>
2022-08-06 13:41:55 +0530

    Code refactor

--- src/main/java/com/service/CryptoPricesTrackingService.java
+++ src/main/java/com/service/CryptoPricesTrackingService.java
@@ -6,5 +6,6 @@ import java.util.List;
 
 public interface CryptoPricesTrackingService {
     void checkAndReportPrice();
+
     List<BitcoinData> getPriceDetails(String date, Integer limit, Integer offset);
 }
--- src/test/java/com/CryptoPricesTrackerApplicationTest.java
+++ src/test/java/com/CryptoPricesTrackerApplicationTest.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CryptoPricesTrackerApplicationTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 }
